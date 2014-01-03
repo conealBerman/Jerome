d0739b20e85afeb43ddff82d4187764c61e8c372
conealBerman <coneal@bermangrp.com>
2014-01-03 18:00:19 -0500

    First draft of site

---
+++ views/output_person_item.php
@@ -0,0 +1,8 @@
+<div class="person">
+	<h2 class="name"><span class="first_name"></span> <span class="last_name"></span></h2>
+	<h3 class="work_title"></h3>
+	<h4 class="work_email"><a href=""></a></h4>
+	<h3 class="work_phone"></h5>
+	<img class="person_image" src="" alt="">
+	<p class="bio"></p>
+</div>
